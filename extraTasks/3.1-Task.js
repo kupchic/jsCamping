2b7e93b2f906ab80fc3e5bca9094d44037146785
Kupchenya Aleksandr <kup4enya1999@gmail.com>
2020-11-08 20:40:11 +0300

    extraTasks3 ready

---
+++ extraTasks/3.1-Task.js
@@ -0,0 +1,11 @@
+let searchMaxSum = (arr) => {
+  let maxSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let tempSum = 0;
+    for (let j = i; j < arr.length; j++) {
+    tempSum += arr[j];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  }
+  return maxSum;
+};
---
+++ extraTasks/3.2-Task.js
@@ -0,0 +1,13 @@
+let getProfit = (arr) => {
+  arr = arr.filter(a=>{
+    return a>0&&a;
+  });
+  if (arr.length < 2) return 0;
+  let profit = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] > 0) {
+      profit += arr[i] - arr[i - 1];
+    }
+  }
+  return profit;
+};
