30a46d4a9101d309d2e8d2541e7f4225656e5b7b
Kupchenya Aleksandr <kup4enya1999@gmail.com>
2020-11-08 20:02:59 +0300

    last fix!

--- extraTasks/3.1-Task.js
+++ extraTasks/3.1-Task.js
@@ -1,10 +1,12 @@
 let searchMaxSum = (arr) => {
   let maxSum = 0;
   let tempSum = 0;
-  for (let i = 0; i <= arr.length; i++) {
-    tempSum += arr[i];
+  for (let i = 0; i < arr.length; i++) {
+    let tempSum = 0;
+    for (let j = i; j < arr.length; j++) {
+    tempSum += arr[j];
     maxSum = Math.max(maxSum, tempSum);
-    if (tempSum < 0 || tempSum < maxSum) tempSum = 0;
   }
   return maxSum;
+}
 };
